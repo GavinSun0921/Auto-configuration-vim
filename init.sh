3f35033abfc75b013d95b7b53210f18b79914d58
Gavin Sun <gainsun0921@foxmail.com>
2022-02-08 08:26:01 +0800

    fix init.sh => illegal line feed

--- init.sh
+++ init.sh
@@ -7,7 +7,7 @@ echo "Get new '~/.vimrc'"
 cp .vimrc ~/.vimrc
 if [ ! -f "~/.vim/autoload/plug.vim" ];then
     echo "Downloading vim-plug from github"
-    curl -fLo ~/.vim/autoload/plug.vim --create-dirs \ 
+    curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
 
